3ade558183ea1eda51a445f73f779b003ffeb49d
Mustafa YALNIZ <mustafa.yalnizz@gmail.com>
2016-11-19 23:47:05 +0200

    Fix unifying problem with  restructure map.

---
+++ src/test/java/LandStructureUtilsTest.java
@@ -0,0 +1,45 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by mylnz on 19.11.2016.
+ * LandStructureUtilsTest
+ */
+public class LandStructureUtilsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getCityConnectionMap() throws Exception {
+
+    }
+
+    @Test
+    public void getNodeSet() throws Exception {
+
+    }
+
+    @Test
+    public void getNodeSet1() throws Exception {
+
+    }
+
+    @Test
+    public void getLeafCitySet() throws Exception {
+
+    }
+
+    @Test
+    public void getMaxLeafCount() throws Exception {
+
+    }
+
+}
